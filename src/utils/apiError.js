e2d76c66a82850356907a23e3ec0ff67b675bd69
adii1203 <aditya32ft@gmail.com>
2023-12-01 14:39:32 +0530

    bug fix

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -5,11 +5,12 @@ class apiError extends Error {
         errors = [],
         stack = ''
     ) {
-        super(message);
+        super();
         this.message = message;
         this.statusCode = statusCode;
         this.errors = errors;
         this.data = null;
+        this.success = false;
 
         if (stack) {
             this.stack = stack;
