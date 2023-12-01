f47fd45f5325aa9c2915974ead6fdc2774835836
adii1203 <aditya32ft@gmail.com>
2023-12-01 14:04:19 +0530

    apiresponce handler function

---
+++ src/utils/apiResponce.js
@@ -0,0 +1,10 @@
+class apiResponce {
+    constructor(statusCode, message = 'success', data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = true;
+    }
+}
+
+export { apiResponce };
