46cf25056e74697ee3c045f64dc416d4c2d1784b
adii1203 <aditya32ft@gmail.com>
2023-12-11 21:18:49 +0530

    fix: cookie-parser config, route setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-
+import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(express.json({ limit: '20kb' }));
 app.use(
     cors({
-        origin: '*',
+        origin: 'http://127.0.0.1:5173',
         credentials: true,
     })
 );
+app.use(cookieParser());
 
+import linkRouter from './routes/link.route.js';
+
+app.use('/', linkRouter);
 export default app;
