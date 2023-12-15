e728f04889b051cde5acb1bccacdbc29aa8a6c6d
adii1203 <aditya32ft@gmail.com>
2023-12-16 00:27:04 +0530

    feat: Add password encryption and token generation methods to User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
-
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const UserSchema = new Schema(
     {
         email: {
@@ -27,6 +28,40 @@ const UserSchema = new Schema(
     },
     { timestamps: true }
 );
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+UserSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+    const accessToken = jwt.sign(
+        { _id: this._id, email: this.email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRE,
+        }
+    );
+    return accessToken;
+};
+
+UserSchema.methods.generateRefreshToken = function () {
+    const refreshToken = jwt.sign(
+        { _id: this._id },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+        }
+    );
+    return refreshToken;
+};
 
 const User = mongoose.model('User', UserSchema);
 
