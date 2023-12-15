ec6524cc9e31b3a8070b3c9c02003008c315c2b6
adii1203 <aditya32ft@gmail.com>
2023-12-16 00:28:08 +0530

    feat: Add auth middleware for token verification

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from '../utils/asyncHandler';
+import { apiError } from '../utils/apiError';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model';
+
+export const verifyToken = asyncHandler(async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+
+        if (!token) {
+            return res.status(401).json(new apiError(401, 'token not found'));
+        }
+        const decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+        if (!decodedToken) {
+            return res.status(401).json(new apiError(402, 'Unauthorized user'));
+        }
+        const user = await User.findById(decodedToken?._id).select(
+            '-password -refreshToken'
+        );
+        if (!user) {
+            return res.status(401).json(new apiError(401, 'Unauthorized'));
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(500).json(new apiError(500, error.message));
+    }
+});
