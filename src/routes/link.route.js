d4eb9ef0ed0eabd20ea6e628e2b2a2e4fec88592
adii1203 <aditya32ft@gmail.com>
2023-12-23 17:47:44 +0530

    Add getStats route to link.route.js

--- src/routes/link.route.js
+++ src/routes/link.route.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { shortLink, getAllLinks } from '../controllers/link.controller.js';
+import {
+    shortLink,
+    getAllLinks,
+    getStats,
+} from '../controllers/link.controller.js';
 import { verifyToken } from '../middlewares/auth.middleware.js';
 const router = Router();
 
 // Secure Routes
 router.route('/short').post(verifyToken, shortLink);
 router.route('/fatchlinks').get(verifyToken, getAllLinks);
+router.route('/stats/:key').get(verifyToken, getStats);
 
 export default router;
